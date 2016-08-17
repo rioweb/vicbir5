1a16dc103ac91d1656ff784a6d245e421bdb78e6
Luis <luisfg25>
2016-08-16 23:42:34 -0300

    16/8

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,7 +14,7 @@ var main = function(){
   /* global $ */
 setTimeout(function() {
     $('#preloader').fadeOut('100');
-  },5500);
+  },50);
 
 
 $('.nav li, .nav li a').click(function (e) {
@@ -35,8 +35,13 @@ $('#closer').click(function(){
 if(window.matchMedia('(max-width: 480px)').matches) {
   var weight = $(window).height();
   $('.fullheight').css('height', weight);
-  
 };
+    
+ $('.carousel-inner').carousel({
+  interval: 9999999999999
+});
+    
+     
 $(function(){
   
     $('.nav li a[href*= "#" ]:not([href= "#" ])').click(function() {
