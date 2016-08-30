117b1fd653475abfd94c6d6cd8bda772a9af99ba
Luis <luisfg25>
2016-08-29 23:54:29 -0300

    a

--- app/send.php
+++ app/send.php
@@ -1,12 +1,12 @@
 <?php
 if(isset($_POST["nombre"]) && isset($_POST["telefono"]) && isset($_POST["comentario"]) ){
-$to = "contacto@rioweb.com.ar";
+$to = "contacto@victoriabirchner.com.ar";
 $subject = "Mensaje Enviado";
 $contenido .= "Nombre: ".$_POST["nombre"]."\n";
 $contenido .= "Email: ".$_POST["email"]."\n\n";
 $contenido .= "Email: ".$_POST["telefono"]."\n\n";
 $contenido .= "Email: ".$_POST["comentario"]."\n\n";
-$header = "From: rioweb@eresselvaconsultoranatura.com\nReply-To:".$_POST["email"]."\n";
+$header = "From: contacto@victoriabirchner.com.ar\nReply-To:".$_POST["email"]."\n";
 $header .= "Mime-Version: 1.0\n";
 $header .= "Content-Type: text/plain";
 if(mail($to, $subject, $contenido ,$header)){
